a7f7762e0a5ff212247df09fe7e369e726d8904e
vanmanDev <vanmandev@gmail.com>
2024-09-05 11:10:19 +0700

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,18 +1,13 @@
-import Vuex from 'vuex';
+ import Vuex from 'vuex';
 
 const store = new Vuex.Store({
     state: {
-        host:'https://back-end-production-d6d6.up.railway.app/'
-    },
-    mutations: {
-    },
-    actions: {
-    },
-    modules: {
-    },
-    getters: {
-    }
+        host:'https://back-end-production-d6d6.up.railway.app/',
+    }, 
+    mutations: {},
+    actions: {},
+    modules: {},
+    getters: {}
 });
 
 export default store;
-
